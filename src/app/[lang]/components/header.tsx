329b033731f36ad3f593cbad67bb8d95aabde09a
Manuel Alférez <manuelalferezruiz@gmail.com>
2023-10-25 10:30:09 +0200

    feat: installed radix ui and personalized button in the header

--- src/app/[lang]/components/header.tsx
+++ src/app/[lang]/components/header.tsx
@@ -2,6 +2,7 @@
 
 import { locales } from "@/app/middleware";
 import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
 
 export default function Header() {
   const router = useRouter();
@@ -12,13 +13,13 @@ export default function Header() {
   return (
     <div className="flex justify-end gap-2">
       {locales.map((locale: string) => (
-        <button
-          className="rounded-md border-2 border-gray-300 p-2 px-4"
+        <Button
+          variant="outline"
           onClick={() => changeLang(locale)}
           key={locale}
         >
           {locale.toUpperCase()}
-        </button>
+        </Button>
       ))}
     </div>
   );
